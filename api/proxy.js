26faf33ded453c4751c0e0300f0d07219d49b991
MateoPinilla <jesus.pinilla@ariadnacg.com>
2025-01-16 09:23:01 -0500

    Authorization

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default async function handler(req, res){
     res.setHeader('Access-Control-Allow-Origin', 'https://distoyotaequiposindustriales.com/');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
